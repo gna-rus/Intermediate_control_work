17ac9185e1471dd727745aba31f018e004409254
gna-rus <gnarus@inbox.ru>
2023-06-20 17:34:41 +0300

    2 commit

--- Task1_python/main.py
+++ Task1_python/main.py
@@ -14,30 +14,54 @@ import datetime
 
 def menu():
     print("add - добавить элемент в заметки")
+    print("read - прочитать все заметки")
+    print("clear - удалить все заметки")
+
+# Функция генерация нового Id
+def findId(nums):
+    with open("note.json", "r", encoding="utf-8") as note:
+        if nums == 0:
+            return 1
+        else:
+            nums = json.load(note)
+            print(nums)
+            return nums+1
+
+# Добавляю элемент в заметки
+def addNote(noteTitle, bodyTitle, timeJson):
+    newNote = dict()
+
+    # записываю в файл данные
+    with open("note.json", "a", encoding="utf-8") as note:
+        newNote[1] = [noteTitle, bodyTitle, timeJson]
+        json.dump(newNote, note, indent=2, sort_keys=True)
+
 menu()
-vremya = datetime.datetime.now() #Вызов метода now из класса datetime.
-print(vremya) #Вывод времени на экран
+vremya = datetime.datetime.now()  # Вызов метода now из класса datetime.
+timeJson = vremya.strftime("%m/%d/%Y | %H:%M:%S")
+print(timeJson)  # Вывод времени на экран
 
 # Формат заметки {id:[заголовок, тело, дата\время]}
+
 # comand = input("Введите команду: ")
 # noteTitle = input("Введите заголовок заметки: ")
 # bodyTitle = input("Введите тело заметки: ")
 
+# Меню ввода
 newNote = dict()
 comand = "add"
-noteTitle = "Head"
-bodyTitle = "Body"
-newNote[noteTitle] = bodyTitle
-# записываю в файл данные
-with open("note.json", "w", encoding="utf-8") as note:
-    json.dump(newNote, note, indent=2, sort_keys=True)
+if comand == "add":
+    noteTitle = "Head"
+    bodyTitle = "Body"
+    addNote(noteTitle, bodyTitle, timeJson)
+
     # newJson = json.dumps(newNote)
     # note.write(newJson)
     # print(type(newJson))
 
 # Прочтение заметки
 with open("note.json") as file:
-    data = json.load(file)                # передаем файловый объект
+    data = json.load(file)  # передаем файловый объект
     for key, value in data.items():
         if type(value) == list:
             print(f'{key}: {", ".join(value)}')
