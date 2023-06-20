9312ed8af1f2a6403f94de09926662fbf48b675c
gna-rus <gnarus@inbox.ru>
2023-06-20 16:54:27 +0300

    2 commit

--- Task1_python/main.py
+++ Task1_python/main.py
@@ -10,24 +10,36 @@
 # последующим вводом данных, как-то ещё, на усмотрение студента.
 
 import json
+import datetime
+
 def menu():
     print("add - добавить элемент в заметки")
-
 menu()
+vremya = datetime.datetime.now() #Вызов метода now из класса datetime.
+print(vremya) #Вывод времени на экран
+
+# Формат заметки {id:[заголовок, тело, дата\время]}
 # comand = input("Введите команду: ")
 # noteTitle = input("Введите заголовок заметки: ")
 # bodyTitle = input("Введите тело заметки: ")
+
 newNote = dict()
 comand = "add"
 noteTitle = "Head"
 bodyTitle = "Body"
 newNote[noteTitle] = bodyTitle
+# записываю в файл данные
 with open("note.json", "w", encoding="utf-8") as note:
-    json.dump(newNote, note, indent=2, sort_keys=True) # записываю в файл данные
+    json.dump(newNote, note, indent=2, sort_keys=True)
     # newJson = json.dumps(newNote)
     # note.write(newJson)
     # print(type(newJson))
 
-
-
-print(type(newNote))
+# Прочтение заметки
+with open("note.json") as file:
+    data = json.load(file)                # передаем файловый объект
+    for key, value in data.items():
+        if type(value) == list:
+            print(f'{key}: {", ".join(value)}')
+        else:
+            print(f'{key}: {value}')
