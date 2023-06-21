fc2fac415599b48e2dc0ef639dcc5683b23226b6
gna-rus <gnarus@inbox.ru>
2023-06-21 14:13:34 +0300

    work version 0.1

--- Task1_python/main.py
+++ Task1_python/main.py
@@ -13,11 +13,14 @@ import json
 import datetime
 import os
 
+
 def menu():
     print("add - добавить элемент в заметки")
     print("read - прочитать по id или все заметки")
     print("edit - редактировать по id")
     print("clear - удалить по id или все заметки")
+    print("exit - Выход")
+
 
 # Функция Даты и Времени в данный момент
 def DTofNote():
@@ -31,9 +34,11 @@ def miniMenu():
         for key, value in data.items():
             print(f'{key} - {value[0]}')
 
+
 # Функция генерация нового Id
 def findId(dictJson):
     return len(dictJson) + 1
+
 
 # Добавляю элемент в заметки
 def addNote(noteTitle, bodyTitle, timeJson):
@@ -45,7 +50,7 @@ def addNote(noteTitle, bodyTitle, timeJson):
     dictJson.update(newNote)  # добавляю в словарь новый элемент
 
     json.dump(dictJson, open("note.json", 'w'), indent=2, ensure_ascii=False)  # перевожу словарь в Json
-                                                                    # (ensure_ascii - позволяет записывать кирилицу)
+    # (ensure_ascii - позволяет записывать кирилицу)
     print(f"Заметка доабвлена!")
 
 
@@ -61,14 +66,24 @@ def readJson(nums):
                 print(f'{value[2:]}')
         elif nums in data.keys():  # выводим на экран заметки по id
             print(f'{int(nums)}: {data[nums][0]}')
-            print(f'{data[nums][1]}')
+            # далее код для многострочного вывода в командную строку
+            count = 0
+            for i in data[nums][1]:
+                if (i == " ") and (count > 100):
+                    count = 0
+                    print()
+                else:
+                    count += 1
+                    print(i, sep="", end="")
+            print()
             print(f'{data[nums][2:]}')
         else:
             print("Error!!! Не корректный id")
 
+
 # Функция редактирования заметок
 def editJson(num):
-    newDickJson = dict() # словарь-буфер
+    newDickJson = dict()  # словарь-буфер
     flag = False
     # проверка, есть ли вообще такое id в файле
     with open("note.json") as file:
@@ -79,14 +94,14 @@ def editJson(num):
     if flag == True:
         with open("note.json", "r") as file1:
             dictJson = json.load(file1)
-            newNoteTitle = "Голова!!!"
-            newBodyTitle = "Тело!!!!"
+            newNoteTitle = input("Введите новый заголовок: ")
+            newBodyTitle = input("Заметка: ")
         # цикл для поиска id
         for key, value in dictJson.items():
             print(key, value)
             if key != num:
                 newDickJson[key] = value
-            else: # если совпадение найдено то вводим корректировку в Заголовок и Тело заметки
+            else:  # если совпадение найдено то вводим корректировку в Заголовок и Тело заметки
                 value[0], value[1], value[2] = newNoteTitle, newBodyTitle, DTofNote()
                 newDickJson[key] = value
         with open('note.json', 'w') as file:
@@ -97,7 +112,7 @@ def editJson(num):
 
 # Функция для удаления id или всех заметок
 def clearJson(num):
-    newDickJson = dict()  #словарь-буфер
+    newDickJson = dict()  # словарь-буфер
     flag = False
     # проверка, есть ли вообще такое id в файле
     with open("note.json") as file:
@@ -116,33 +131,41 @@ def clearJson(num):
             json.dump(newDickJson, file, indent=2, ensure_ascii=False)
     elif num == "all":
         with open("note.json", "w") as file1:
-            json.dump(dict(), file1, indent=2, ensure_ascii=False) #записываю в файл пустой словарь
+            json.dump(dict(), file1, indent=2, ensure_ascii=False)  # записываю в файл пустой словарь
             print("Все заметки удалены!")
     else:
         print("Невернный ввод! Такой id не найден")
 
-menu()
+
+
 print(DTofNote())  # Вывод времени на экран
 
 # Меню ввода
-newNote = dict()
-comand = "add"
-if comand == "add":
-    noteTitle = "Голова"
-    bodyTitle = "Тело"
-    addNote(noteTitle, bodyTitle, DTofNote())
-elif comand == "read":
-    miniMenu()
-    nums = input("Введите номер id (или all для вывода всех заметок) ")
-    readJson(nums)
-elif comand == "edit":
-    miniMenu()
-    nums = input("Введите номер id для редактирования ")
-    editJson(nums)
-elif comand == "clear":
-    miniMenu()
-    nums = input("Введите номер id (или all для удаления всех заметок) ")
-    clearJson(nums)
-
+comand = ""
+while (comand != "exit"):
+    menu()
+    comand = input("Введите команду: ")
+    if comand == "add":
+        noteTitle = input("Введите заголовок: ")
+        bodyTitle = input("Заметка: ")
+        addNote(noteTitle, bodyTitle, DTofNote())
+        input()
+    elif comand == "read":
+        miniMenu()
+        nums = input("Введите номер id (или all для вывода всех заметок) ")
+        readJson(nums)
+        input()
+    elif comand == "edit":
+        miniMenu()
+        nums = input("Введите номер id для редактирования ")
+        editJson(nums)
+        input()
+    elif comand == "clear":
+        miniMenu()
+        nums = input("Введите номер id (или all для удаления всех заметок) ")
+        clearJson(nums)
+    elif comand == "exit":
+        print("Досвидание!")
+        break
 
 sizeOfDictJson = os.path.getsize("note.json")  # определяю пустой файл или нет (в файле должно быть хотя бы "{}")
